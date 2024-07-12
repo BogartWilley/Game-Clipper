f15b2314a862c3e6790bd5be349cbb473efb3bf8
Salim <152934001+SalimOfShadow@users.noreply.github.com>
2024-07-13 00:03:42 +0200

    Send inital python get request

--- js-script/server/server.js
+++ js-script/server/server.js
@@ -1,46 +1,46 @@
-const { obs } = require('../obs-api/websocketApi.js');
-const express = require('express');
+const { obs } = require("../obs-api/websocketApi.js");
+const express = require("express");
 const app = express();
 const PORT = 4609;
-const recordingRoutes = require('./routes/routes.js');
+const recordingRoutes = require("./routes/routes.js");
 // Action imports
 
-const waitKeyPress = require('../utils/actions/waitKeyPress.js');
-const handleErrors = require('../utils/handleErrors.js');
+const waitKeyPress = require("../utils/actions/waitKeyPress.js");
+const handleErrors = require("../utils/handleErrors.js");
 
 app.use(express.json());
-app.use('/', recordingRoutes);
+app.use("/", recordingRoutes);
 
-app.get('/test', (req, res) => {
-	res.status(200).send({
-		message: `env variable set correctly ${process.env.SELECTED_GAME}`,
-	});
+app.get("/test", (req, res) => {
+  res.status(200).send({
+    message: `env variable set correctly ${process.env.SELECTED_GAME}`,
+  });
 });
-obs.on('RecordStateChanged', (data) => {
-	console.log('Record state changed:', data);
-	if (!data.outputActive && data.outputPath !== null) {
-		console.log('Recording has finished converting.');
-		// Perform actions after recording has finished converting
-	}
+obs.on("RecordStateChanged", (data) => {
+  console.log("Record state changed:", data);
+  if (!data.outputActive && data.outputPath !== null) {
+    console.log("Recording has finished converting.");
+    // Perform actions after recording has finished converting
+  }
 });
 
 // Starts the web server once connected to the websocket
-obs.on('ConnectionOpened', async () => {
-	try {
-		app.listen(4609, () => {
-			console.log(`Server listening on port 4609`);
-			// TODO: Move it to websocketApi.js
-		});
-	} catch (err) {
-		handleErrors(err);
-	}
+obs.on("ConnectionOpened", async () => {
+  try {
+    app.listen(4609, () => {
+      console.log(`Server listening on port 4609`);
+      // TODO: Move it to websocketApi.js
+    });
+  } catch (err) {
+    handleErrors(err);
+  }
 });
 
 // Exit the process on keypress
 async function exit() {
-	console.log("Press 'Q' to exit...");
-	await waitKeyPress('q');
-	process.exit();
+  console.log("Press 'Q' to exit...");
+  await waitKeyPress("q");
+  process.exit();
 }
 exit();
 

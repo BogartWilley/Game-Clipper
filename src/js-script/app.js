5b6bec733530d464e03df7481b13ec425d8a9e87
Your NameSalim <salimofshadow@outlook.it>
2024-07-24 23:09:44 +0200

    add simple electron app

---
+++ src/js-script/app.js
@@ -0,0 +1,32 @@
+const { default: OBSWebSocket } = require('obs-websocket-js');
+const obs = new OBSWebSocket();
+const connectWs = require('./utils/connection/connect.js');
+const launchObs = require('./utils/connection/launchObs.js');
+require('dotenv').config({ path: '../../.env' }); // Reads .env from root
+
+const isGameRunning = true; // TODO - Close OBS and the script on game close
+
+// Starts OBS process
+
+const obsProcess = launchObs();
+
+// Launching the main app
+
+const main = async () => {
+	const isConnected = await connectWs(obs);
+	if (!isConnected) {
+		setTimeout(async () => {
+			try {
+				console.log('Reconnecting...');
+				await main(); // Await the recursive call to prevent overlapping
+			} catch (err) {
+				console.log(err);
+			}
+		}, 3000);
+		return;
+	}
+};
+
+main();
+
+module.exports = { obs, main };
---
+++ src/js-script/utils/handleErrors.js
@@ -0,0 +1,49 @@
+const selectedGame = process.env.SELECTED_GAME;
+function handleErrors(err, cb) {
+	// Adds colored error messages
+	console.log(`\x1b[31m--- ERROR: \x1b[33m${err.code}\x1b[31m ---\x1b[0m`);
+	function printError(message) {
+		console.log(`\x1b[1;31m${message}\x1b[0m`);
+	}
+	switch (err.code) {
+		case 207: {
+			printError('The server is not ready to handle the request.');
+			console.log('Retrying...');
+			setTimeout(() => {
+				console.log('FIRING createNewScene()');
+				cb;
+			}, 500);
+			break;
+		}
+		case 500:
+			printError(
+				'An output is running and cannot be in order to perform the request.'
+			);
+		case 501:
+			printError('An output is not running and should be.');
+			break;
+		case 502:
+			printError('An output is paused and should not be.');
+			break;
+		case 503:
+			printError('An output is not paused and should be.');
+			break;
+		case 504:
+			printError('An output is disabled and should not be.');
+			break;
+		case 505:
+			printeError('Studio mode is active and cannot be.');
+			break;
+		case 506:
+			printError('Studio mode is not active and should be.');
+			break;
+		case 601:
+			printError('A source already exists by that scene name.');
+			break;
+		default:
+			console.log(err);
+	}
+}
+module.exports = handleErrors;
+
+// Refer to https://github.com/obsproject/obs-websocket/blob/master/docs/generated/protocol.json

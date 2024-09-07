c07f67f418ebded74be3e4d9e2608d33c0e5b6da
Salim <152934001+SalimOfShadow@users.noreply.github.com>
2024-09-07 19:58:31 +0200

    Delete py-script directory

--- py-script/utils/send_action.py
+++
@@ -1,8 +0,0 @@
-import requests
-def send_action(game,action):
-    url = f"http://localhost:4609/{action}-recording"
-    headers = {
-        "game": game
-    }
-    response = requests.get(url, headers=headers)
-    return response

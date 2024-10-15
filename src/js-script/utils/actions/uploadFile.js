a33125bf9158e65bb9eb039d1d5b3cdc313ba2b2
Salim <salimofshadow@outlook.it>
2024-10-15 16:26:45 +0200

    add visibility field to toggle unlisted video flag

--- src/js-script/utils/actions/uploadFile.js
+++ src/js-script/utils/actions/uploadFile.js
@@ -5,19 +5,23 @@ const pathToVideo = 'C:\\Users\\salim\\Videos\\test.mp4';
 async function uploadFile(filePath) {
   try {
     const currentGame = process.env.CURRENT_GAME || 'KOF XIII';
-    const currentUser = process.env.CURRENT_USERNAME || 'SalimOfShadow';
+    const currentUser = process.env.CURRENT_USERNAME || 'Guest';
+    const visibility = process.env.VISIBILITY || 'public'; // TODO - IMPLEMENT THIS
     const fileName = `${currentGame.replace(/_/g, '')} Match Replay | ${currentUser}`;
+
     const currentEnv = process.env.CURRENT_ENV || 'dev';
     const endpointURL = 'http://localhost:6001';
     //const endpointURL = 'https://salimkof.pro:6001';
     // const endpointURL = currentEnv === 'dev'
     //   ? 'http://localhost:3001'
     //   : 'https://salimkof.pro:3001';
+
     console.log(`About to upload a file from this path : ${filePath}`);
     const file = await fs.openAsBlob(filePath);
     const formData = new FormData();
     formData.set('currentGame', currentGame);
     formData.set('currentUser', currentUser);
+    formData.set('visibility', visibility);
     formData.set('replay', file, 'TEST'); // TODO - CHANGE TEST & FIGURE OUT WHAT'S WRONG
     const response = await fetch(`${endpointURL}/recieve-video`, {
       method: 'POST',

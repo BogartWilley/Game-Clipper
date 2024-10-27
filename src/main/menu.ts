f3ca4cbd5e4b4b7acc7b7e77ebf8f4421509e18e
Salim <salimofshadow@outlook.it>
2024-10-28 00:53:34 +0100

    change project's name from Rekka to Recca

--- src/main/menu.ts
+++ src/main/menu.ts
@@ -57,14 +57,14 @@ export default class MenuBuilder {
       label: 'Electron',
       submenu: [
         {
-          label: 'About Rekka',
+          label: 'About Recca',
           selector: 'orderFrontStandardAboutPanel:',
         },
         { type: 'separator' },
         { label: 'Services', submenu: [] },
         { type: 'separator' },
         {
-          label: 'Hide Rekka',
+          label: 'Hide Recca',
           accelerator: 'Command+H',
           selector: 'hide:',
         },

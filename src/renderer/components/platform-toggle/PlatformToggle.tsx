8f385a68b178570cc33f5c21e12d5f631bb1317a
Salim <salimofshadow@outlook.it>
2024-11-01 21:11:58 +0100

    add animation to youtube and streamable buttons

--- src/renderer/components/platform-toggle/PlatformToggle.tsx
+++ src/renderer/components/platform-toggle/PlatformToggle.tsx
@@ -1,6 +1,7 @@
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import { useState } from 'react';
+import { motion } from 'framer-motion';
 const youtubeLogo = require('./platform-images/youtube-logo.png');
 const streamableLogo = require('./platform-images/stream-logo.png');
 type PossiblePlatform = 'youtube' | 'streamable';
@@ -14,54 +15,63 @@ export default function PlatformToggle(props: any) {
   }
 
   return (
-    <Box sx={{ display: 'flex', '& button': { m: 1 } }}>
-      {' '}
-      {/* Changed from div to Box */}
-      <Button
-        size="medium"
-        variant="contained"
-        style={{
-          backgroundColor:
-            selectedPlatform === 'youtube' ? '#9e0000' : '#9e000090',
-          userSelect: 'none',
-        }}
-        onClick={() => {
-          handlePlatformToggle('youtube');
-        }}
-        disabled={selectedPlatform === 'youtube'}
-        draggable="false"
-      >
-        <img
-          width="120px"
-          src={youtubeLogo}
-          alt="YouTube Logo"
-          style={{ userSelect: 'none', pointerEvents: 'none' }}
-          draggable="false"
-        />
-      </Button>
-      <Button
-        size="medium"
-        variant="contained"
-        style={{
-          backgroundColor:
-            selectedPlatform === 'streamable' ? '#0072a7' : '#0072a790',
-          userSelect: 'none',
-        }}
-        onClick={() => {
-          handlePlatformToggle('streamable');
-        }}
-        disabled={selectedPlatform === 'streamable'}
-        draggable="false"
-      >
-        <img
-          width="120px"
-          height="38px"
-          src={streamableLogo}
-          alt="Streamable Logo"
-          style={{ userSelect: 'none', pointerEvents: 'none' }}
-          draggable="false"
-        />
-      </Button>
-    </Box>
+    <motion.div>
+      <Box sx={{ display: 'flex', '& button': { m: 1 } }}>
+        <motion.div
+          whileHover={selectedPlatform === 'streamable' ? { scale: 1.07 } : {}}
+          whileTap={selectedPlatform === 'streamable' ? { scale: 0.95 } : {}}
+        >
+          <Button
+            size="medium"
+            variant="contained"
+            style={{
+              backgroundColor:
+                selectedPlatform === 'youtube' ? '#b91616' : '#b9161690',
+            }}
+            onClick={() => {
+              handlePlatformToggle('youtube');
+            }}
+            disabled={selectedPlatform === 'youtube'}
+            draggable="false"
+          >
+            <img
+              width="120px"
+              src={youtubeLogo}
+              alt="YouTube Logo"
+              style={{ userSelect: 'none', pointerEvents: 'none' }}
+              draggable="false"
+            />
+          </Button>
+        </motion.div>
+
+        <motion.div
+          whileHover={selectedPlatform === 'youtube' ? { scale: 1.07 } : {}}
+          whileTap={selectedPlatform === 'youtube' ? { scale: 0.95 } : {}}
+        >
+          <Button
+            size="medium"
+            variant="contained"
+            style={{
+              backgroundColor:
+                selectedPlatform === 'streamable' ? '#3088b0' : '#3088b090',
+            }}
+            onClick={() => {
+              handlePlatformToggle('streamable');
+            }}
+            disabled={selectedPlatform === 'streamable'}
+            draggable="false"
+          >
+            <img
+              width="120px"
+              height="38px"
+              src={streamableLogo}
+              alt="Streamable Logo"
+              style={{ userSelect: 'none', pointerEvents: 'none' }}
+              draggable="false"
+            />
+          </Button>
+        </motion.div>
+      </Box>
+    </motion.div>
   );
 }
